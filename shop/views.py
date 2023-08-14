d54efc27a47166ff5fde51fa4d247c4afe359560
i-panin-19 <ilypanin@gmail.com>
2023-08-15 03:38:29 +0700

    add files for hw4

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from shop.models import Product, Category
+
+
+def index(request):
+    context = {
+        'product_list': Product.objects.all(),
+        'category_list': Category.objects.all(),
+        'title': 'Магазин "Ромашка"',
+    }
+    return render(request, 'shop/index.html', context)
